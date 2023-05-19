ef55a5084611afa393242e23e56e9c915580a8d9
sibmaks <sibmaks@vk.com>
2023-05-19 23:42:03 +0300

    Add rubber block-items

--- src/main/java/com/github/sib_energy_craft/rubber/load/Blocks.java
+++ src/main/java/com/github/sib_energy_craft/rubber/load/Blocks.java
@@ -7,14 +7,16 @@ import com.github.sib_energy_craft.rubber.block.RubberSaplingGenerator;
 import com.github.sib_energy_craft.rubber.block.RubberStrippedLogBlock;
 import com.github.sib_energy_craft.sec_utils.common.Identified;
 import com.github.sib_energy_craft.sec_utils.load.DefaultModInitializer;
-import com.github.sib_energy_craft.sec_utils.utils.BlockUtils;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.entity.EntityType;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.Direction;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
+
+import static com.github.sib_energy_craft.sec_utils.utils.BlockUtils.register;
 
 
 /**
@@ -35,6 +37,13 @@ public final class Blocks implements DefaultModInitializer {
     public static final Identified<PillarBlock> RUBBER_WOOD;
     public static final Identified<PillarBlock> STRIPPED_RUBBER_WOOD;
 
+    public static final Identified<ButtonBlock> RUBBER_BUTTON;
+    public static final Identified<FenceBlock> RUBBER_FENCE;
+    public static final Identified<FenceGateBlock> RUBBER_FENCE_GATE;
+    public static final Identified<PressurePlateBlock> RUBBER_PRESSURE_PLATE;
+    public static final Identified<SlabBlock> RUBBER_SLAB;
+    public static final Identified<StairsBlock> RUBBER_STAIRS;
+
     static {
         var leavesSettings = FabricBlockSettings.of(Material.LEAVES)
                 .sounds(BlockSoundGroup.GRASS)
@@ -45,7 +54,7 @@ public final class Blocks implements DefaultModInitializer {
                 .suffocates((state, world, pos) -> false)
                 .blockVision((state, world, pos) -> false);
 
-        RUBBER_LEAVES = BlockUtils.register(Identifiers.of("rubber_leaves"), new LeavesBlock(leavesSettings));
+        RUBBER_LEAVES = register(Identifiers.of("rubber_leaves"), new LeavesBlock(leavesSettings));
 
        var saplingSettings = FabricBlockSettings.of(Material.PLANT)
                .noCollision()
@@ -54,39 +63,69 @@ public final class Blocks implements DefaultModInitializer {
                .breakInstantly()
                .sounds(BlockSoundGroup.GRASS);
         var saplingBlock = new SaplingBlock(new RubberSaplingGenerator(), saplingSettings);
-        RUBBER_SAPLING = BlockUtils.register(Identifiers.of("rubber_sapling"), saplingBlock);
+        RUBBER_SAPLING = register(Identifiers.of("rubber_sapling"), saplingBlock);
 
         var pottedSaplingSettings = FabricBlockSettings.of(Material.DECORATION)
                 .breakInstantly()
                 .nonOpaque();
         var flowerPotBlock = new FlowerPotBlock(RUBBER_SAPLING.entity(), pottedSaplingSettings);
-        POTTED_RUBBER_SAPLING = BlockUtils.register(Identifiers.of("potted_rubber_sapling"), flowerPotBlock);
+        POTTED_RUBBER_SAPLING = register(Identifiers.of("potted_rubber_sapling"), flowerPotBlock);
 
-        var rubberLogSettings = getLogSettings(MapColor.DIRT_BROWN, MapColor.SPRUCE_BROWN);
+        var rubberLogSettings = getLogSettings(MapColor.WHITE_GRAY, MapColor.SPRUCE_BROWN);
         var rubberLogBlock = new RubberLogBlock(rubberLogSettings);
-        RUBBER_LOG = BlockUtils.register(Identifiers.of("rubber_log"), rubberLogBlock);
+        RUBBER_LOG = register(Identifiers.of("rubber_log"), rubberLogBlock);
 
         var strippedRubberLogSettings = getWoodSettings(MapColor.OAK_TAN, MapColor.OAK_TAN);
         var strippedLogBlock = new RubberStrippedLogBlock(strippedRubberLogSettings);
-        STRIPPED_RUBBER_LOG = BlockUtils.register(Identifiers.of("stripped_rubber_log"), strippedLogBlock);
+        STRIPPED_RUBBER_LOG = register(Identifiers.of("stripped_rubber_log"), strippedLogBlock);
 
         var planksSettings = AbstractBlock.Settings.of(Material.WOOD, MapColor.BROWN)
                 .strength(2.0f, 3.0f)
                 .sounds(BlockSoundGroup.WOOD);
         var rubberPlank = new Block(planksSettings);
-        RUBBER_PLANKS = BlockUtils.register(Identifiers.of("rubber_planks"), rubberPlank);
+        RUBBER_PLANKS = register(Identifiers.of("rubber_planks"), rubberPlank);
 
-        var rubberWoodSettings = getWoodSettings(MapColor.DIRT_BROWN, MapColor.DIRT_BROWN);
+        var rubberWoodSettings = getWoodSettings(MapColor.WHITE_GRAY, MapColor.WHITE_GRAY);
         var rubberWood = new PillarBlock(rubberWoodSettings);
-        RUBBER_WOOD = BlockUtils.register(Identifiers.of("rubber_wood"), rubberWood);
+        RUBBER_WOOD = register(Identifiers.of("rubber_wood"), rubberWood);
 
         var strippedRubberWood = new PillarBlock(strippedRubberLogSettings);
-        STRIPPED_RUBBER_WOOD = BlockUtils.register(Identifiers.of("stripped_rubber_wood"), strippedRubberWood);
+        STRIPPED_RUBBER_WOOD = register(Identifiers.of("stripped_rubber_wood"), strippedRubberWood);
 
         Hooks.AxeItemClassInit.accept(Map.of(
                 Blocks.RUBBER_LOG.entity(), Blocks.STRIPPED_RUBBER_LOG.entity(),
                 Blocks.RUBBER_WOOD.entity(), Blocks.STRIPPED_RUBBER_WOOD.entity()
         ));
+
+        var rubberItemsSettings = AbstractBlock.Settings.of(Material.WOOD, MapColor.WHITE_GRAY)
+                .strength(2.0F, 3.0F)
+                .sounds(BlockSoundGroup.WOOD);
+
+        var rubberBlockSetType = BlockSetType.BIRCH;
+        var rubberButton = createWoodenButtonBlock(rubberBlockSetType);
+        RUBBER_BUTTON = register(Identifiers.of("rubber_button"), rubberButton);
+
+        var rubberFence = new FenceBlock(rubberItemsSettings);
+        RUBBER_FENCE = register(Identifiers.of("rubber_fence"), rubberFence);
+
+        var rubberFenceGate = new FenceGateBlock(rubberItemsSettings, WoodType.BIRCH);
+        RUBBER_FENCE_GATE = register(Identifiers.of("rubber_fence_gate"), rubberFenceGate);
+
+        var rubberPressurePlateSettings = AbstractBlock.Settings
+                .of(Material.WOOD, RUBBER_PLANKS.entity().getDefaultMapColor())
+                .noCollision()
+                .strength(0.5F);
+        var rubberPressurePlate = new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING,
+                rubberPressurePlateSettings,
+                rubberBlockSetType);
+        RUBBER_PRESSURE_PLATE = register(Identifiers.of("rubber_pressure_plate"), rubberPressurePlate);
+
+        var rubberSlabBlock = new SlabBlock(rubberItemsSettings);
+        RUBBER_SLAB = register(Identifiers.of("rubber_slab"), rubberSlabBlock);
+
+        var rubberStairsBlock = new StairsBlock(RUBBER_PLANKS.entity().getDefaultState(),
+                AbstractBlock.Settings.copy(RUBBER_PLANKS.entity()));
+        RUBBER_STAIRS = register(Identifiers.of("rubber_stairs"), rubberStairsBlock);
     }
 
     private static AbstractBlock.Settings getWoodSettings(MapColor topMapColor, MapColor sideMapColor) {
@@ -102,8 +141,15 @@ public final class Blocks implements DefaultModInitializer {
                 .ticksRandomly();
     }
 
-    private static boolean isUpDownFacing(BlockState state) {
+    private static boolean isUpDownFacing(@NotNull BlockState state) {
         var axisFacing = state.get(RubberLogBlock.AXIS_FACING);
         return axisFacing.axis == Direction.Axis.Y;
+    }
+
+    private static ButtonBlock createWoodenButtonBlock(@NotNull BlockSetType blockSetType) {
+        return new ButtonBlock(AbstractBlock.Settings.of(Material.DECORATION)
+                .noCollision()
+                .strength(0.5F), blockSetType, 30, true
+        );
     }
 }
--- src/main/java/com/github/sib_energy_craft/rubber/load/Items.java
+++ src/main/java/com/github/sib_energy_craft/rubber/load/Items.java
@@ -2,6 +2,7 @@ package com.github.sib_energy_craft.rubber.load;
 
 import com.github.sib_energy_craft.energy_api.utils.Identifiers;
 import com.github.sib_energy_craft.sec_utils.load.DefaultModInitializer;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.util.Rarity;
@@ -15,20 +16,27 @@ import static com.github.sib_energy_craft.sec_utils.utils.ItemUtils.registerBloc
  * @author sibmaks
  */
 public final class Items implements DefaultModInitializer {
-    public static final Item RUBBER_LEAVES;
+    public static final BlockItem RUBBER_LEAVES;
 
-    public static final Item RUBBER_SAPLING;
+    public static final BlockItem RUBBER_SAPLING;
 
-    public static final Item RUBBER_LOG;
-    public static final Item STRIPPED_RUBBER_LOG;
+    public static final BlockItem RUBBER_LOG;
+    public static final BlockItem STRIPPED_RUBBER_LOG;
 
-    public static final Item RUBBER_PLANKS;
+    public static final BlockItem RUBBER_PLANKS;
 
-    public static final Item RUBBER_WOOD;
-    public static final Item STRIPPED_RUBBER_WOOD;
+    public static final BlockItem RUBBER_WOOD;
+    public static final BlockItem STRIPPED_RUBBER_WOOD;
 
     public static final Item RUBBER;
     public static final Item VULCANIZED_RUBBER;
+
+    public static final BlockItem RUBBER_BUTTON;
+    public static final BlockItem RUBBER_FENCE;
+    public static final BlockItem RUBBER_FENCE_GATE;
+    public static final BlockItem RUBBER_PRESSURE_PLATE;
+    public static final BlockItem RUBBER_SLAB;
+    public static final BlockItem RUBBER_STAIRS;
 
     static {
         RUBBER_SAPLING = registerBlockItem(ItemGroups.NATURAL, Blocks.RUBBER_SAPLING);
@@ -39,6 +47,13 @@ public final class Items implements DefaultModInitializer {
         RUBBER_PLANKS = registerBlockItem(ItemGroups.BUILDING_BLOCKS, Blocks.RUBBER_PLANKS);
         RUBBER_WOOD = registerBlockItem(ItemGroups.BUILDING_BLOCKS, Blocks.RUBBER_WOOD);
         STRIPPED_RUBBER_WOOD = registerBlockItem(ItemGroups.BUILDING_BLOCKS, Blocks.STRIPPED_RUBBER_WOOD);
+
+        RUBBER_BUTTON = registerBlockItem(ItemGroups.REDSTONE, Blocks.RUBBER_BUTTON);
+        RUBBER_FENCE = registerBlockItem(ItemGroups.BUILDING_BLOCKS, Blocks.RUBBER_FENCE);
+        RUBBER_FENCE_GATE = registerBlockItem(ItemGroups.REDSTONE, Blocks.RUBBER_FENCE_GATE);
+        RUBBER_PRESSURE_PLATE = registerBlockItem(ItemGroups.REDSTONE, Blocks.RUBBER_PRESSURE_PLATE);
+        RUBBER_STAIRS = registerBlockItem(ItemGroups.BUILDING_BLOCKS, Blocks.RUBBER_STAIRS);
+        RUBBER_SLAB = registerBlockItem(ItemGroups.BUILDING_BLOCKS, Blocks.RUBBER_SLAB);
 
         var commonItem = new Item.Settings()
                 .rarity(Rarity.COMMON);
