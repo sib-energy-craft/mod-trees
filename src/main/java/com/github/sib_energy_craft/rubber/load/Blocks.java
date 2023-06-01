c6883b6a1874869849166c64e1e6f76d19e86593
sibmaks <sibmaks@vk.com>
2023-06-01 22:34:04 +0300

    Update Minecraft up to 1.20

--- src/main/java/com/github/sib_energy_craft/rubber/load/Blocks.java
+++ src/main/java/com/github/sib_energy_craft/rubber/load/Blocks.java
@@ -7,8 +7,9 @@ import com.github.sib_energy_craft.rubber.block.RubberStrippedLogBlock;
 import com.github.sib_energy_craft.sec_utils.Hooks;
 import com.github.sib_energy_craft.sec_utils.common.Identified;
 import com.github.sib_energy_craft.sec_utils.load.DefaultModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.entity.EntityType;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.Direction;
@@ -47,29 +48,35 @@ public final class Blocks implements DefaultModInitializer {
     public static final Identified<TrapdoorBlock> RUBBER_TRAPDOOR;
 
     static {
-        var leavesSettings = FabricBlockSettings.of(Material.LEAVES)
+        var leavesSettings = AbstractBlock.Settings.create()
+                .mapColor(MapColor.GREEN)
                 .sounds(BlockSoundGroup.GRASS)
                 .strength(0.2f)
                 .ticksRandomly()
                 .nonOpaque()
                 .allowsSpawning((state, world, pos, type) -> type == EntityType.OCELOT || type == EntityType.PARROT)
                 .suffocates((state, world, pos) -> false)
-                .blockVision((state, world, pos) -> false);
+                .blockVision((state, world, pos) -> false)
+                .burnable()
+                .pistonBehavior(PistonBehavior.DESTROY)
+                .solidBlock((state, world, pos) -> false);
 
         RUBBER_LEAVES = register(Identifiers.of("rubber_leaves"), new LeavesBlock(leavesSettings));
 
-       var saplingSettings = FabricBlockSettings.of(Material.PLANT)
-               .noCollision()
-               .nonOpaque()
-               .ticksRandomly()
-               .breakInstantly()
-               .sounds(BlockSoundGroup.GRASS);
+        var saplingSettings =  AbstractBlock.Settings.create()
+                .mapColor(MapColor.GREEN)
+                .noCollision()
+                .ticksRandomly()
+                .breakInstantly()
+                .sounds(BlockSoundGroup.GRASS)
+                .pistonBehavior(PistonBehavior.DESTROY);
         var saplingBlock = new SaplingBlock(new RubberSaplingGenerator(), saplingSettings);
         RUBBER_SAPLING = register(Identifiers.of("rubber_sapling"), saplingBlock);
 
-        var pottedSaplingSettings = FabricBlockSettings.of(Material.DECORATION)
+        var pottedSaplingSettings = AbstractBlock.Settings.create()
                 .breakInstantly()
-                .nonOpaque();
+                .nonOpaque()
+                .pistonBehavior(PistonBehavior.DESTROY);
         var flowerPotBlock = new FlowerPotBlock(RUBBER_SAPLING.entity(), pottedSaplingSettings);
         POTTED_RUBBER_SAPLING = register(Identifiers.of("potted_rubber_sapling"), flowerPotBlock);
 
@@ -81,9 +88,12 @@ public final class Blocks implements DefaultModInitializer {
         var strippedLogBlock = new RubberStrippedLogBlock(strippedRubberLogSettings);
         STRIPPED_RUBBER_LOG = register(Identifiers.of("stripped_rubber_log"), strippedLogBlock);
 
-        var planksSettings = AbstractBlock.Settings.of(Material.WOOD, MapColor.BROWN)
+        var planksSettings = AbstractBlock.Settings.create()
+                .mapColor(MapColor.ORANGE)
+                .instrument(Instrument.BASS)
                 .strength(2.0f, 3.0f)
-                .sounds(BlockSoundGroup.WOOD);
+                .sounds(BlockSoundGroup.WOOD)
+                .burnable();
         var rubberPlank = new Block(planksSettings);
         RUBBER_PLANKS = register(Identifiers.of("rubber_planks"), rubberPlank);
 
@@ -99,21 +109,28 @@ public final class Blocks implements DefaultModInitializer {
                 Blocks.RUBBER_WOOD.entity(), Blocks.STRIPPED_RUBBER_WOOD.entity()
         ));
 
-        var rubberItemsSettings = AbstractBlock.Settings.of(Material.WOOD, MapColor.WHITE_GRAY)
-                .strength(2.0F, 3.0F)
-                .sounds(BlockSoundGroup.WOOD);
-
         var rubberBlockSetType = BlockSetType.BIRCH;
 
-        var rubberDoorSettings = AbstractBlock.Settings
-                .of(Material.WOOD, RUBBER_PLANKS.entity().getDefaultMapColor())
+        var rubberDoorSettings = AbstractBlock.Settings.create()
+                .mapColor(RUBBER_PLANKS.entity().getDefaultMapColor())
+                .instrument(Instrument.BASS)
                 .strength(3.0F)
-                .nonOpaque();
+                .nonOpaque()
+                .burnable()
+                .pistonBehavior(PistonBehavior.DESTROY);
         var rubberDoor = new DoorBlock(rubberDoorSettings, rubberBlockSetType);
         RUBBER_DOOR = register(Identifiers.of("rubber_door"), rubberDoor);
 
         var rubberButton = createWoodenButtonBlock(rubberBlockSetType);
         RUBBER_BUTTON = register(Identifiers.of("rubber_button"), rubberButton);
+
+        var rubberItemsSettings = AbstractBlock.Settings.create()
+                .mapColor(RUBBER_PLANKS.entity().getDefaultMapColor())
+                .solid()
+                .instrument(Instrument.BASS)
+                .strength(2.0F, 3.0F)
+                .sounds(BlockSoundGroup.WOOD)
+                .burnable();
 
         var rubberFence = new FenceBlock(rubberItemsSettings);
         RUBBER_FENCE = register(Identifiers.of("rubber_fence"), rubberFence);
@@ -121,10 +138,14 @@ public final class Blocks implements DefaultModInitializer {
         var rubberFenceGate = new FenceGateBlock(rubberItemsSettings, WoodType.BIRCH);
         RUBBER_FENCE_GATE = register(Identifiers.of("rubber_fence_gate"), rubberFenceGate);
 
-        var rubberPressurePlateSettings = AbstractBlock.Settings
-                .of(Material.WOOD, RUBBER_PLANKS.entity().getDefaultMapColor())
+        var rubberPressurePlateSettings =  AbstractBlock.Settings.create()
+                .mapColor(RUBBER_PLANKS.entity().getDefaultMapColor())
+                .solid()
+                .instrument(Instrument.BASS)
                 .noCollision()
-                .strength(0.5F);
+                .strength(0.5F)
+                .burnable()
+                .pistonBehavior(PistonBehavior.DESTROY);
         var rubberPressurePlate = new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING,
                 rubberPressurePlateSettings,
                 rubberBlockSetType);
@@ -137,26 +158,34 @@ public final class Blocks implements DefaultModInitializer {
                 AbstractBlock.Settings.copy(RUBBER_PLANKS.entity()));
         RUBBER_STAIRS = register(Identifiers.of("rubber_stairs"), rubberStairsBlock);
 
-        var rubberTrapdoorSettings = AbstractBlock.Settings
-                .of(Material.WOOD, MapColor.PALE_YELLOW)
+        var rubberTrapdoorSettings = AbstractBlock.Settings.create()
+                .mapColor(MapColor.PALE_YELLOW)
+                .instrument(Instrument.BASS)
                 .strength(3.0F)
                 .nonOpaque()
-                .allowsSpawning((state, world, pos, type) -> false);
+                .allowsSpawning((state, world, pos, type) -> false)
+                .burnable();
         var rubberTrapdoor = new TrapdoorBlock(rubberTrapdoorSettings, rubberBlockSetType);
         RUBBER_TRAPDOOR = register(Identifiers.of("rubber_trapdoor"), rubberTrapdoor);
     }
 
     private static AbstractBlock.Settings getWoodSettings(MapColor topMapColor, MapColor sideMapColor) {
-        return FabricBlockSettings.of(Material.WOOD, state -> state.get(PillarBlock.AXIS) == Direction.Axis.Y ? topMapColor : sideMapColor)
+        return AbstractBlock.Settings.create()
+                .mapColor(state -> state.get(PillarBlock.AXIS) == Direction.Axis.Y ? topMapColor : sideMapColor)
+                .instrument(Instrument.BASS)
                 .strength(2.f)
-                .sounds(BlockSoundGroup.WOOD);
+                .sounds(BlockSoundGroup.WOOD)
+                .burnable();
     }
 
     private static AbstractBlock.Settings getLogSettings(MapColor topMapColor, MapColor sideMapColor) {
-        return FabricBlockSettings.of(Material.WOOD, state -> isUpDownFacing(state) ? topMapColor : sideMapColor)
+        return AbstractBlock.Settings.create()
+                .mapColor(state -> isUpDownFacing(state) ? topMapColor : sideMapColor)
+                .instrument(Instrument.BASS)
                 .strength(2.f)
                 .sounds(BlockSoundGroup.WOOD)
-                .ticksRandomly();
+                .ticksRandomly()
+                .burnable();
     }
 
     private static boolean isUpDownFacing(@NotNull BlockState state) {
@@ -165,9 +194,11 @@ public final class Blocks implements DefaultModInitializer {
     }
 
     private static ButtonBlock createWoodenButtonBlock(@NotNull BlockSetType blockSetType) {
-        return new ButtonBlock(AbstractBlock.Settings.of(Material.DECORATION)
+        var buttonSettings = AbstractBlock.Settings.create()
                 .noCollision()
-                .strength(0.5F), blockSetType, 30, true
-        );
+                .strength(0.5F)
+                .pistonBehavior(PistonBehavior.DESTROY);
+
+        return new ButtonBlock(buttonSettings, blockSetType, 30, true);
     }
 }
