10daaefd595d783630a8543bb96a95aeb4c8ee53
sibmaks <sibmaks@vk.com>
2023-06-09 12:13:06 +0300

    Tree tap only from interaction side

--- src/main/java/com/github/sib_energy_craft/rubber/block/FilledSide.java
+++ src/main/java/com/github/sib_energy_craft/rubber/block/FilledSide.java
@@ -1,6 +1,8 @@
 package com.github.sib_energy_craft.rubber.block;
 
+import lombok.AllArgsConstructor;
 import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Locale;
@@ -9,12 +11,17 @@ import java.util.Locale;
  * @author sibmaks
  * @since 0.0.9
  */
+@AllArgsConstructor
 public enum FilledSide implements StringIdentifiable {
-    NONE,
-    A,
-    B,
-    C,
-    D;
+    NONE(null, null, null),
+    A(Direction.NORTH, Direction.NORTH, Direction.UP),
+    B(Direction.SOUTH, Direction.SOUTH, Direction.DOWN),
+    C(Direction.DOWN, Direction.EAST, Direction.EAST),
+    D(Direction.UP, Direction.WEST, Direction.WEST);
+
+    public final Direction xSide;
+    public final Direction ySide;
+    public final Direction zSide;
 
     @NotNull
     @Override
